44fa6a4573d5ef796b69b7250bbf218b821acc3a
Affan <AffanR237@gmail.com>
2025-03-26 14:04:58 +0500

    Working project UDP client recieving data from the server pasing it in JSON and checking if its updated. IF the data is chenged then only pass it to the SDK to upload to the controller card.

---
+++ build/CMakeFiles/SDKNetApp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Projects/Dot_matrix_led_panel/Communication_with_LED_Panel_HD-D16/SDK 3.0_TestVersion2/SDKNetApi/src/main.cpp" "CMakeFiles/SDKNetApp.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SDKNetApp.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
